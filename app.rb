5937b8a382004f6214c543d644f11a2cffe43464
aburk3 <aburk3@gmail.com>
2018-12-01 16:03:03 -0600

    Done.

--- app.rb
+++ app.rb
@@ -13,9 +13,11 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase'  do
+    my_string = ""
     @phrase = params[:phrase]
     (params[:number].to_i).times do
-      puts "#{@phrase}"
+      my_string += "#{@phrase}\n"
     end
+    my_string
   end
 end
