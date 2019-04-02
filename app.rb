c3eab825671c5ba91668850abab4f0ac868d8edc
Diana Badas <diana.badas@gmail.com>
2019-04-02 13:37:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,54 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @reversed_name = params[:name].reverse!
+    "#{@reversed_name}"
+  end
+
+  get '/square/:number' do
+    @url_number = params[:number].to_i
+    @squared_number = @url_number * @url_number
+    @squared_number.to_s
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    @result = []
+
+    @number.times do |x|
+      @result << @phrase
+    end
+
+    @result
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+
+    case @operation
+    when "add"
+      "#{@number1 + @number2}".to_s
+    when "subtract"
+      "#{@number1 - @number2}".to_s
+    when "multiply"
+      "#{@number1 * @number2}".to_s
+    when "divide"
+      "#{@number1 / @number2}".to_s
+    end
+  end
 
 end
