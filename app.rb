68a774b724d1aaa7c6b9bd5fcad7aef3bdc6e7d8
J. Singleton <jennifer.singleton@gmail.com>
2019-04-11 02:05:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,44 +3,38 @@ require 'pry'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    @name = params[:name]
-    "#{@name.reverse}"
+    "#{params[:name].reverse}"
   end
 
   get '/square/:number' do
-    @num = params[:number].to_i
-    "#{@num * @num}"
+    "#{params[:number].to_i ** 2}"
   end
 
   get '/say/:number/:phrase' do
-    @phrase = params[:phrase]
     @num = params[:number].to_i
+
     @repeated_phrase = []
-    @num.times {@repeated_phrase << @phrase}
+    @num.times {@repeated_phrase << params[:phrase]}
+
     "#{@repeated_phrase.join}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @word1 = params[:word1]
-    @word2 = params[:word2]
-    @word3 = params[:word3]
-    @word4 = params[:word4]
-    @word5 = params[:word5]
-    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get '/:operation/:number1/:number2' do
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
-    @operation = params[:operation]
 
-    if @operation == "add"
+    case params[:operation]
+    when "add"
       @result = @num1 + @num2
-    elsif @operation == "subtract"
+    when "subtract"
       @result = @num1 - @num2
-    elsif @operation == "divide"
+    when "divide"
       @result = @num1 / @num2
-    elsif @operation == "multiply"
+    when "multiply"
       @result = @num1 * @num2
     end
 
