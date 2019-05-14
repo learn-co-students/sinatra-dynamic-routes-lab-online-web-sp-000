f7f22fab83087022da945fd53733360d47438337
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-14 14:06:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -23,4 +23,25 @@ class App < Sinatra::Base
     @phrase
   end
   # binding.pry
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @phrase = "#{params[:word1]} " + "#{params[:word2]} " + "#{params[:word3]} " + "#{params[:word4]} " + "#{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+
+    if @operation == "add"
+      (@num1 + @num2).to_s
+    elsif @operation == "subtract"
+      (@num1 - @num2).to_s
+    elsif @operation == "multiply"
+      (@num1 * @num2).to_s
+    elsif @operation == "divide"
+      # binding.pry
+      (@num1 / @num2).to_s
+    end
+  end
+
 end
