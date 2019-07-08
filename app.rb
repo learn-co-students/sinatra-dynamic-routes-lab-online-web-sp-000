5ce41015991ca22945bf71ccd1f608cb01d8cf31
Jared Grove <jaredkgrove@gmail.com>
2019-07-08 16:51:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,51 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get "/reversename/:name" do
+    @reversename = params[:name].reverse!
+    @reversename
+  end
+
+  get "/square/:number" do
+    @number = params[:number]
+    "#{@number.to_f ** 2}"
+  end
+
+  get "/say/:number/:phrase" do
+    @number = params[:number]
+    @phrase = params[:phrase]
+
+    @phrase * @number.to_i
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_f
+    @number2 = params[:number2].to_f
+
+    case @operation
+    when "add"
+      "#{@number1 + @number2}"
+    when "subtract"
+      "#{@number1 - @number2}"
+    when "divide"
+      "#{@number1 / @number2}"
+    when "multiply"
+      "#{@number1 * @number2}"
+    end
+  end
 
 end
+# Create a dynamic route at get '/say/:word1/:word2/:word3/:word4/:word5' that accepts five words and returns a string containing all five words (i.e. word1 word2 word3 word4 word5).
+#
+# Create a dynamic route at get '/:operation/:number1/:number2' that accepts an operation (add, subtract, multiply or divide) and performs the operation on the two numbers provided. For example, going to /add/1/2 should render 3.
