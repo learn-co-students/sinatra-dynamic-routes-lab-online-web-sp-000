409e40621fd5803f696a61f4cd91b1a6a69ae69d
Frederic Silber <freddy.silber@cloudcoach.com>
2019-09-06 00:58:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -24,4 +24,34 @@ class App < Sinatra::Base
     end
     "#{phrases}"
   end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    case @operation
+    when "add"
+        @sum = @number1 + @number2
+        "#{@sum}"
+      when "subtract"
+        @difference = @number1 - @number2
+        "#{@difference}"
+      when "multiply"
+        @product = @number1 * @number2
+        "#{@product}"
+      when "divide"
+        @qotient = @number1 / @number2
+        "#{@qotient}"
+      else
+    end
+  end
 end
