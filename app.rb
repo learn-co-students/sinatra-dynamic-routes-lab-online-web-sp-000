9298b2a3b1738b0d2dd1111a5c3df4d8d0e6ead4
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-27 20:21:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,25 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  
+  get '/reservename/:name' do 
+    <%@name =params[:name].reverse%>
+    @name
+  end
+  
+  get '/square/:number' do
+    <%@number = params[:number].to_i%>
+    <%@number = sqrt(@number)%>
+    @number
+  end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    <%word1=params[:word1]%>
+    <%word2=params[:word2]%>
+    <%word3=params[:word3]%>
+    <%word4=params[:word4]%>
+    <%word5=params[:word5]%>
+    <%="#{@word1}#{@word2}#{@word3}#{@word4}#{@word5}"
+  end 
+  
 end
