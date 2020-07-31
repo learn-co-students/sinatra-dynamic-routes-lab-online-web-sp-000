7651f2d8515a9c7231272bc40826fd331d087b80
yarvinh <yarvinhhernandez@gmail.com>
2020-07-30 20:36:18 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+    get '/reversename/:name' do
+      erb :reversename
+    end
 
+    get '/square/:number' do
+      erb :square
+    end
+    get '/say/:number/:phrase' do
+      erb :say
+    end
+
+    get '/say/:word1/:word2/:word3/:word4/:word5' do
+      erb :say
+    end
+
+    get '/:operation/:number1/:number2' do 
+      erb :index
+    end
 end
