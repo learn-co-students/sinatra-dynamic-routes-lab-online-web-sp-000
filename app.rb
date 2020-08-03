de1baddb57df649ff656d04b4743b6113e56529a
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-08-03 03:09:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,19 +12,28 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    binding.pry
+    statement = ' '
+    params[:number].to_i.times { statement += params[:phrase]}
+    statement
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @word1 = params[:word1]
-    @word2 = params[:word2]
-    @word3 = params[:word3]
-    @word4 = params[:word4]
-    @word5 = params[:word5]
-    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  get '/' do
+  get '/:operation/:number1/:number2' do
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    case params[:operation]
+      when 'add'
+        answer = (number1 + number2).to_s
+      when 'subtract'
+        answer = (number1 - number2).to_s
+      when 'multiply'
+        answer = (number1 * number2).to_s
+      when 'divide'
+        answer = (number1 / number2).to_s
+      end
+    end
 
-  end
 end
