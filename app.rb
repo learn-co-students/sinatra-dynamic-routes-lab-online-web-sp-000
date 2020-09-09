7652c81bac9f4485364f60a715c7153891c03773
sparsell <hello@sparsell.com>
2020-09-09 06:21:27 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    @name = params[:name]
+    @in_reverse = @name.reverse
+    "#{@in_reverse}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number]
+    @number.to_i
+    @sq_root = Math.sqrt(@number)
+    @sq_root.to_s
+    "#{@sq_root}"
+  end
 
 end
