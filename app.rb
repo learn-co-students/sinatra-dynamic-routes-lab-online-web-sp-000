9b83519465a082f28b831dc23e110e075c776821
sparsell <hello@sparsell.com>
2020-09-09 10:19:46 -0400

    Done.

--- app.rb
+++ app.rb
@@ -10,10 +10,41 @@ class App < Sinatra::Base
 
   get '/square/:number' do
     @number = params[:number]
-    @number.to_i
-    @square = @number * @number
-    @square.to_s
+    @square = @number.to_i * @number.to_i
     "#{@square}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    "#{@phrase}"*@number
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+      @word1 = params[:word1]
+      @word2 = params[:word2]
+      @word3 = params[:word3]
+      @word4 = params[:word4]
+      @word5 = params[:word5]
+      "#{@word1} " + "#{@word2} " + "#{@word3} " + "#{@word4} " + "#{@word5}" + "."
+  end
+
+
+  get '/:operation/:number1/:number2' do
+      @operation = params[:operation]
+      @num1 = params[:number1].to_i
+      @num2 = params[:number2].to_i
+      case @operation
+      when "add"
+        @sum = @num1 + @num2
+      when "subtract"
+        @sum = @num1 - @num2
+      when "multiply"
+        @sum = @num1 * @num2
+      when "divide"
+        @sum = @num1 / @num2
+        end
+        "#{@sum}"
   end
 
 end
