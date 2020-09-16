3dfa9dc868ba13bc73c76db25e44450a85668e1d
reevesalic <reevesalic@gmail.com>
2020-09-16 11:50:50 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+ get '/hello' do
+   "Hello"
+ end
+  
+
+  get '/reversename' do
+    @name = params[:name]
+    "Hello #{name.reverse}"
+  end
 end
