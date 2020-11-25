2ce578b23f5569b5a1da1d9c494f1168c2ea624b
JohannesHupfeld <j.hupfeld@gmail.com>
2020-11-25 14:27:04 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,42 +3,36 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    params[:name].reverse
+    reversed = params[:name].reverse
+    "#{reversed}"
   end
 
   get '/square/:number' do
-    (params[:number].to_i ** 2).to_s
+    square = params[:number].to_i * params[:number].to_i
+    "#{square}"
   end
 
   get '/say/:number/:phrase' do
-    ans = ''
-
-    params[:number].to_i.times do
-      ans += params[:phrase]
-    end
-
-    ans
+    full_phrase = params[:phrase] * params[:number].to_i
+    "#{full_phrase}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+      "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  get '/:operation/:number1/:number2' do
-    number1 = params[:number1].to_i
-    number2 = params[:number2].to_i
-
-    ans = 'Unable to perform this operation'
-
+   get '/:operation/:number1/:number2' do
     case params[:operation]
     when 'add'
-      ans = (number1 + number2).to_s
+      result = params[:number1].to_i + params[:number2].to_i
     when 'subtract'
-      ans = (number1 - number2).to_s
-    when 'multiply'
-      ans = (number1 * number2).to_s
+      result = params[:number1].to_i - params[:number2].to_i
     when 'divide'
-      ans = (number1 / number2).to_s
+      result = params[:number1].to_i / params[:number2].to_i
+    when 'multiply'
+      result = params[:number1].to_i * params[:number2].to_i
     end
+    "#{result}"
   end
+
 end
