3bb542049e70e92210b3ab604721d13fba6cc880
carolyn-manning <carolyn.manning3@gmail.com>
2021-03-26 02:16:49 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/reversename/:name' do
-    params[:name].split("").reverse.join
+    params[:name].reverse
   end
 
   get '/square/:number' do
@@ -17,24 +17,21 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @word_array = [params[:word1], params[:word2], params[:word3],
-    params[:word4], params[:word5]]
-    @word_array.join(" ") << "."
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get '/:operation/:number1/:number2' do
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
-    @integer = if params[:operation] == "add"
-      @num1 + @num2 
+    if params[:operation] == "add"
+      (@num1 + @num2).to_s
     elsif params[:operation] == "multiply"
-      @num1 * @num2 
+      (@num1 * @num2).to_s
     elsif params[:operation] == "subtract"
-      @num1 - @num2 
+      (@num1 - @num2).to_s 
     elsif params[:operation] == "divide"
-      @num1 / @num2 
+      (@num1 / @num2).to_s
     end
-    @integer.to_s
   end
 
 
