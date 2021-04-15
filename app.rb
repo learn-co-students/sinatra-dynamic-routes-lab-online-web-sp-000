8fdc9f87b4db282dc019e876c5d9f63e05db9b9b
clone18476 <cameroncousins18@gmail.com>
2021-04-15 16:32:45 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,55 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get '/' do
+    "Hello World"
+  end
+  
+  get "/reversename/:name" do
+    user_name = params[:name].reverse
+    user_name
+  end
+
+  get "/square/:number" do
+    number = params[:number].to_i
+    num_output = number * number
+    num_output.to_s
+  end
+
+  get "/say/:number/:phrase" do
+    number = params[:number].to_i
+    phrase = params[:phrase]
+    [phrase] * number
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @W1 = params[:word1]
+    @W2 = params[:word2]
+    @W3 = params[:word3]
+    @W4 = params[:word4]
+    @W5 = params[:word5]
+    output = "#{@W1} #{@W2} #{@W3} #{@W4} #{@W5}."
+    output
+  end
+
+  get "/:operation/:number1/:number2" do
+    operation = params[:operation]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    if operation == "add"
+      output = number1 + number2
+      return output.to_s
+    elsif operation == "subtract"
+      output = number1 - number2
+      return output.to_s
+    elsif operation == "multiply"
+      output = number1 * number2
+      return output.to_s
+    else operation == "divide"
+      output = number1 / number2
+      return output.to_s
+    end
+  end
 
 end
