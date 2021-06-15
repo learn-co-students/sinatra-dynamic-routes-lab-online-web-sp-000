9ac88c967ef9d7b403d56cf3424770aa9d0272cb
Hannah M <hannahemalm616@gmail.com>
2021-06-15 01:00:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -29,19 +29,25 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end 
   
-  get "/:operation/:number1/:number2" do 
-    @operation = params[:operation]
-    @number1 = params[:number1].to_i
-    @number2 = params[:number2].to_i
-    if @operation == "add"
-      @number1 + @number2
-    elsif @operation == "subtract"
-      @number2 - @number1
-    elsif @operation == "multiply"
-      @number1 * @number2
-    else 
-      @number2 % @number1
-  end 
+  get "/:operation/:number1/:number2" do
+    
+    operation = params[:operation]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+   
+    if operation == "add"  
+     (number1 + number2).to_s  
+   elsif operation == "subtract"  
+       (number1 - number2).to_s  
+   elsif operation == "multiply"  
+     (number1 * number2).to_s  
+   elsif operation == 'divide'  
+     (number1 / number2).to_s  
+   else  
+       'Unable to perform this operation'  
+   end  
+  
 end 
+
 
 end 
